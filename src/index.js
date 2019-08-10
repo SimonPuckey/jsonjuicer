1370f3723a7beca39f3c559660b3b1444bb2aa7a
Simon Puckey <simonpuckey@hotmail.com>
2019-08-10 11:21:42 +0100

    Does not search value if number, could do with explicitly testing this

--- src/index.js
+++ src/index.js
@@ -1,29 +1,11 @@
-function* searchObject (obj, searchTerm) {
-    for(var prop in obj) {
-        if (prop === searchTerm) yield obj[prop];
-        if (prop !== searchTerm && typeof obj[prop] !== 'string'){
-            const searchStrategy = getSearchStrategy(obj[prop]);
-            const json = obj[prop];
-            yield * searchStrategy(json,searchTerm);
-        }
-    }
-};
-
-function* searchArray (arr, searchTerm) {
-    for(const json of arr){
-        const searchStrategy = getSearchStrategy(json); 
-        yield * searchStrategy(json,searchTerm);
-    } 
-};
+//support funcs
+const isObject = (input) => typeof input === 'object' && !Array.isArray(input);
+const isNullOrWhitespace = ( input ) => !input || !input.trim();
+const isSearchable = (val) => isNaN(val) && typeof val !== 'string';
 
 const validateSearchTerm = (searchTerm) => {
     if(isNullOrWhitespace(searchTerm)) throw new Error('Search term must not be empty');
     return searchTerm;
-};
-
-const getSearchStrategy = (data) => {
-    if(isObject(data)) return searchObject;
-    if(Array.isArray(data)) return searchArray;
 };
 
 const getValidSearchStrategy = (data, getSearchStrategy) => {
@@ -32,11 +14,33 @@ const getValidSearchStrategy = (data, getSearchStrategy) => {
     if(!strategy) throw new Error('to be searched JSON element should be object or array');
 }
 
-//utility
-const isObject = (input) => typeof input === 'object' && !Array.isArray(input);
-const isNullOrWhitespace = ( input ) => !input || !input.trim();
+const getSearchStrategy = (data) => {
+    if(isObject(data)) return searchObject;
+    if(Array.isArray(data)) return searchArray;
+};
 
+//search funcs
+function* searchArray (arr, searchTerm) {
+    for(const json of arr){
+        const searchStrategy = getSearchStrategy(json); 
+        yield * searchStrategy(json,searchTerm);
+    } 
+};
 
+function* searchObject (obj, searchTerm) { 
+    for(var key in obj) {
+        const val = obj[key];
+        if (key === searchTerm) yield val;
+        if (isSearchable(val)){
+            const searchStrategy = getSearchStrategy(val);
+            const json = val;
+            console.log("key before failure", key);
+            yield * searchStrategy(json, searchTerm);
+        }
+    }
+};
+
+//main func
 module.exports =  (json,searchFor) => {
     const testResults = [];
     //TODO: restructure & tighten this further?
@@ -46,8 +50,6 @@ module.exports =  (json,searchFor) => {
     for(let x of searchStrategy(json,searchFor)){
         testResults.push(x);
     }
+
     return testResults;
 }
-
-
-
